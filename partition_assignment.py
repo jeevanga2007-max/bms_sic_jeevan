5ce03a3081b53263ff516cb32945b6a9aff9c748
jeevanga2007-max <jeevanga2007@gmail.com>
2026-05-12 16:52:09 +0530

    added the partition program done in class also

--- partition_assignment.py
+++ partition_assignment.py
@@ -15,3 +15,14 @@ def partition(arr):
     arr[pos] = pivot
     return arr
 
+def partition_done_in_class(arr):
+    pivot = arr[-1]
+    i=0
+    j=0
+
+    for i in range(len(arr)-1):
+        if arr[i] <= pivot:
+            arr[i], arr[j] = arr[j], arr[i]
+            j += 1
+    arr[j], arr[len(arr)-1] = arr[len(arr)-1], arr[j]
+    return arr
