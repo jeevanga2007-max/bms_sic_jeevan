1fb0012ba2533f7cba796deca1ff10d835e726ca
jeevanga2007-max <jeevanga2007@gmail.com>
2026-05-26 17:47:11 +0530

    26 may 2026

--- person_crud.py
+++ person_crud.py
@@ -1,3 +1,6 @@
+from itertools import count
+from multiprocessing.dummy import connection
+
 import pymysql
 
 def connect_db():
@@ -16,37 +19,127 @@ def disconnect_db(connection):
         print("DB disconnection failed")
 
 def create_table():
-    query = ' CREATE TABLE people IF NOT EXISTS (id INT PRIMARY KEY AUTO_INCREMENT , name VARCHAR(64) NOT NULL,gender BOOL NOT NULL,age int default(0),location VARCHAR(32));'
+    query = '''
+    CREATE TABLE IF NOT EXISTS people (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(64) NOT NULL,gender BOOL NOT NULL, age INT DEFAULT 0,location VARCHAR(32)); '''
     try:
-        connection=connect_db()
+        connection = connect_db()
         cursor = connection.cursor()
-        count=cursor.execute(query)
-        if count == 0:
-            print("Table created successfully")
-        else:
-            print("Table creation failed")
+        cursor.execute(query)
+        print("Table created successfully")
+        connection.commit()
         cursor.close()
         disconnect_db(connection)
-    except:
+    except Exception as e:
         print("Table creation failed")
+        print(e)
 
+def read_person():
+    name=input("Enter person name: ")
+    age=int(input("Enter person age: "))
+    gender=input("Enter person gender(m/f): ")
+    location=input("Enter person location: ")
+    if gender.lower() == "f":
+        gender = True
+    else:
+        gender = False
+    return (name,gender,age,location)
 
-def create_table():
-    query = ' CREATE TABLE people IF NOT EXISTS (id INT PRIMARY KEY AUTO_INCREMENT , name VARCHAR(64) NOT NULL,gender BOOL NOT NULL,age int default(0),location VARCHAR(32));'
+def create_person():
+    query = """INSERT INTO people(name, gender, age, location) VALUES (%s, %s, %s, %s); """
+
     try:
-        connection=connect_db()
+        person = read_person()
+        connection = connect_db()
         cursor = connection.cursor()
-        count=cursor.execute(query)
-        if count == 0:
-            print("Table created successfully")
+        count = cursor.execute(query, person)
+        connection.commit()
+        print(f"count: {count}")
+        if count == 1:
+            print("Person created successfully")
         else:
-            print("Table creation failed")
+            print("Person creation failed")
+
         cursor.close()
         disconnect_db(connection)
-    except:
-        print("Table creation failed")
 
+    except Exception as e:
+        print("Person creation failed")
+        print(e)
 
+def search_person():
+    id = int(input("Enter person id to be searched: "))
+    query = f'select * from people where id= {id};'
+    try:
+        connection = connect_db()
+        cursor = connection.cursor()
+        cursor.execute(query)
+        print(f"count: {count}")
+        if count == 1:
+            row = cursor.fetchall()
+            print(row)
+            print(type(row))
+        else:
+            print("No people found")
+        connection.commit()
+        cursor.close()
+        disconnect_db(connection)
+    except :
+        print("Failed to list people")
 
+def update_person():
+   id = int(input("Enter person id to be updated: "))
+   new_location = input("Enter new location: ")
+   query = "UPDATE people SET location = %s WHERE id = %s;"
+   try:
+        connection = connect_db()
+        cursor = connection.cursor()
+        cursor.execute(query, (new_location, id))
+        connection.commit()
+        cursor.close()
+        disconnect_db(connection)
+        print()
+        if count == 1:
+            row = cursor.fetchone()
+            print(row)
+            print(type(row))
+        else:
+            print("No people found")
+        connection.commit()
+        cursor.close()
+        disconnect_db(connection)
+    except :
+        print("Failed to list people")
 
-create_table()
+def delete_person():
+    def delete_row(self, id):
+        id = int(input('Enter Id of the person to delete: '))
+        query = f'delete from people where id = {id}'
+        connection = self.connect_db()
+        cursor = connection.cursor()
+        count = cursor.execute(query)
+        if count == 0:
+            print(f'Person with id = {id} not found')
+        else:
+            print(f'Person with id = {id} deleted')
+        connection.commit()
+        cursor.close()
+        self.disconnect_db(connection)
+def list_people():
+    query = 'select * from people;'
+    try:
+        connection = connect_db()
+        cursor = connection.cursor()
+        cursor.execute(query)
+        print(f"count: {count}")
+        if count == 1:
+            row = cursor.fetchone()
+            print(row)
+            print(type(row))
+        else:
+            print("No people found")
+        connection.commit()
+        cursor.close()
+        disconnect_db(connection)
+    except :
+        print("Failed to list people")
+
